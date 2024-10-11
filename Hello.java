9a6b919912e9095074a03dd2d9385ecd1ddcbbdf
pgw20000 <pgw20000@gmail.com>
2024-10-11 16:02:27 +0900

    최초 commit

---
+++ Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+	
+	/**
+	 * 	Hello main 메서드!
+	 */
+	
+	public static void main(String []args){
+		//console : "Hello, world!" 메세지 출력
+		
+		System.out.println("Hello, World!");
+	}
+}
